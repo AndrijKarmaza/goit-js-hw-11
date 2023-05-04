9240a46babb15e5b8b49d8c02608eb46403eaa5e
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-05-04 16:20:51 +0300

    homework is done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '28043876-d01bb93c634d22751608058fc';
+const PHOTO_PER_PAGE = 40;
 
 const searchForm = document.querySelector('#search-form');
 const loadMore = document.querySelector('.load-more');
@@ -12,15 +14,24 @@ loadMore.addEventListener('click', onLoadMore);
 
 let searchQuery;
 let page = 1;
+let loadPhotoNumber = PHOTO_PER_PAGE*page;
 
-function getPhotos () {
-    return axios.get(`${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
-    .then(data => {
-        console.log(data);
-        console.log(page);
-        page += 1;
-        return data;
-    });
+
+
+async function getPhotos () {
+    try {return await axios.get(`${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PHOTO_PER_PAGE}`)
+        .then(data => {
+            loadPhotoNumber = PHOTO_PER_PAGE*page;
+            if (data.data.totalHits && loadPhotoNumber > data.data.totalHits) {
+                loadMore.classList.add('is-hiden');
+                Notify.failure("We're sorry, but you've reached the end of search results.");
+            }
+            page += 1;
+            return data;
+        });
+    } catch (error) {
+              console.error(error);
+    }
 }
 
 function resetPage() {
@@ -29,13 +40,20 @@ function resetPage() {
 
 function onSearch(evt) {
     evt.preventDefault();
+    loadMore.classList.add('is-hiden');
+    clearPhotoMarcup();
     searchQuery = evt.currentTarget.elements.searchQuery.value;
     resetPage();
     getPhotos()
     .then(data => {
         const dataArray = data.data.hits;
-        console.log(dataArray);
+
+        if (!dataArray.length) {
+            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            return;
+        }
         photoGallery.insertAdjacentHTML('beforeend',createPhotoMarcup(dataArray));
+        loadMore.classList.remove('is-hiden');
     });
 }
 
@@ -43,7 +61,6 @@ function onLoadMore() {
     getPhotos()
     .then(data => {
         const dataArray = data.data.hits;
-        console.log(dataArray);
         photoGallery.insertAdjacentHTML('beforeend',createPhotoMarcup(dataArray));
     });
 }
@@ -51,48 +68,24 @@ function onLoadMore() {
 function createPhotoMarcup(arr) {
     return arr.map(({webformatURL, tags, likes, views, comments, downloads} = arr) =>
     `<div class="photo-card">
-<img src="${webformatURL}" alt="${tags}" loading="lazy" />
-<div class="info">
-<p class="info-item">
-<b>Likes</b>${likes}
-</p>
-<p class="info-item">
-<b>Views</b>${views}
-</p>
-<p class="info-item">
-<b>Comments</b>${comments}
-</p>
-<p class="info-item">
-<b>Downloads</b>${downloads}
-</p>
-</div>
-</div>`).join('');
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width = '356px'/>
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b>${likes}
+            </p>
+            <p class="info-item">
+                <b>Views</b>${views}
+            </p>
+            <p class="info-item">
+                <b>Comments</b>${comments}
+            </p>
+            <p class="info-item">
+                <b>Downloads</b>${downloads}
+            </p>
+        </div>
+    </div>`).join('');
 }
 
-
-// async function getUser() {
-//     try {
-//       const response = await axios.get('/user?ID=12345');
-//       console.log(response);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>
+function clearPhotoMarcup() {
+    photoGallery.innerHTML = '';
+}
